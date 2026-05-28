75697aec3f785a4edf227357e11cef625fa52e2c
Mig-Garmor <thecodermiguel@gmail.com>
2026-05-28 16:10:44 +0200

    Cleanup web routes

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'quotation.index');
-Route::view('/login', 'quotation.index')->name('login');
+Route::view('/', 'quotation.index')->name('login');
